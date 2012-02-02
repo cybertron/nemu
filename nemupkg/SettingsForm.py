a811d20d9242ada8a48e2306bbf400aa68dc285f
Ben Nemec <cybertron@nemebean.com>
2012-02-02 09:57:53 -0600

    Add SettingsForm

---
+++ nemupkg/SettingsForm.py
@@ -0,0 +1,180 @@
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import os
+from MenuReader import *
+from MenuItem import *
+
+class SettingsForm(QDialog):
+   def __init__(self, parent = None):
+      QDialog.__init__(self, parent)
+      self.parent = parent
+      self.accepted = False
+      
+      self.setupUI()
+      
+      
+   def setupUI(self):
+      self.setWindowTitle('Nemu Settings')
+      self.mainLayout = QVBoxLayout(self)
+      
+      self.tabs = QTabWidget(self)
+      self.mainLayout.addWidget(self.tabs)
+      
+      importPage = QWidget()
+      importPageLayout = QVBoxLayout(importPage)
+      self.importFileText = QLineEdit()
+      self.importFileSelect = QPushButton('Select')
+      self.importFileSelect.clicked.connect(self.importSelectClicked)
+      self.addWidgets(importPageLayout, 'Menu File', [self.importFileText, self.importFileSelect])
+      
+      themes = self.getIconThemes()
+      self.themeCombo = QComboBox()
+      self.themeCombo.addItems(themes)
+      self.themeCombo.setToolTip('Use icons from this theme if possible')
+      self.addWidgets(importPageLayout, 'Preferred Icon Theme', [self.themeCombo])
+      
+      self.replaceCheck = QCheckBox('Replace Existing')
+      self.replaceCheck.setToolTip('Replace current menu with imported one - otherwise the imported menu will be merged')
+      importPageLayout.addWidget(self.replaceCheck)
+      
+      self.importButton = QPushButton('Import')
+      self.importButton.clicked.connect(self.importClicked)
+      importPageLayout.addWidget(self.importButton)
+      
+      self.importProgress = QProgressBar()
+      importPageLayout.addWidget(self.importProgress)
+      
+      aboutPage = QWidget()
+      aboutPageLayout = QVBoxLayout(aboutPage)
+      aboutText = QTextEdit()
+      aboutText.setAlignment(Qt.AlignCenter)
+      aboutText.append('Nemu\n')
+      aboutText.append('Copyright 2012 Ben Nemec\n')
+      aboutText.append("Distributed under the GPLv3\n")
+      aboutText.append("Web: TBD\n")
+      aboutText.append("E-Mail: cybertron@nemebean.com\n")
+      aboutText.setReadOnly(True)
+      aboutPageLayout.addWidget(aboutText)
+      
+      self.tabs.addTab(importPage, 'Import Menu')
+      self.tabs.addTab(aboutPage, 'About')
+      
+      self.bottomButtonLayout = QHBoxLayout()
+      self.okButton = QPushButton('OK')
+      self.okButton.clicked.connect(self.okClicked)
+      self.bottomButtonLayout.addWidget(self.okButton)
+      
+      self.cancelButton = QPushButton('Cancel')
+      self.cancelButton.clicked.connect(self.close)
+      self.bottomButtonLayout.addWidget(self.cancelButton)
+      
+      self.mainLayout.addLayout(self.bottomButtonLayout)
+      
+      self.resize(500, 250)
+      
+      
+   def addWidgets(self, layout, name, widgets):
+      tempLayout = QHBoxLayout()
+      tempLayout.addWidget(QLabel(name))
+      for i in widgets:
+         tempLayout.addWidget(i)
+      layout.addLayout(tempLayout)
+      
+      
+   def okClicked(self):
+      self.accepted = True
+      self.close()
+      
+      
+   def importSelectClicked(self):
+      filename = QFileDialog.getOpenFileName(directory = '/etc/xdg/menus')
+      if filename != '':
+         self.importFileText.setText(filename)
+         
+         
+   def getIconThemes(self):
+      retval = ['']
+      for i in os.listdir('/usr/share/icons'):
+         currPath = os.path.join('/usr/share/icons', i)
+         if os.path.isdir(currPath):
+            cursor = False
+            for j in os.listdir(currPath):
+               if j == 'cursors':
+                  cursor = True
+            if not cursor:
+               retval.append(i)
+            
+      return retval
+      
+      
+   def importClicked(self):
+      if self.replaceCheck.isChecked():
+         answer = QMessageBox.warning(self, 'Are you sure?', 'This action will erase your current menu.  Continue?', QMessageBox.Yes | QMessageBox.No)
+         if answer != QMessageBox.Yes:
+            return
+            
+      # Our due diligence done, it's time to do the import
+      MenuItem.iconTheme = str(self.themeCombo.currentText())
+      reader = MenuReader(str(self.importFileText.text()))
+      newItems = reader.menuItems
+      self.removeEmptyFolders(newItems)
+      if self.replaceCheck.isChecked():
+         self.parent.menuItems = newItems
+      else:
+         self.mergeMenus(self.parent.menuItems, newItems)
+         
+      # Looking for icons is slow - just do it once
+      self.importProgress.setRange(0, len(self.parent.menuItems))
+      count = 0
+      for i in self.parent.menuItems:
+         self.importProgress.setValue(count)
+         i.findIcon()
+         count += 1
+      self.importProgress.setValue(len(self.parent.menuItems))
+         
+      self.parent.refresh()
+      
+      
+   def removeEmptyFolders(self, items):
+      removed = True
+      while removed:
+         removed = False
+         for i in items:
+            if i.folder:
+               empty = True
+               for j in items:
+                  if j.parent == i:
+                     empty = False
+                     break
+               if empty:
+                  items.remove(i)
+                  removed = True
+      
+      
+   def mergeMenus(self, items, mergeItems):
+      parentMap = dict()
+      for i in mergeItems:
+         if i.parent in parentMap:
+            i.parent = parentMap[i.parent]
+         dup = None
+         for j in items:
+            if self.checkDup(i, j):
+               dup = j
+               break
+         if dup == None:
+            items.append(i)
+         else:
+            parentMap[i] = dup
+            
+            
+   def checkDup(self, i, j):
+      if i.name == j.name:
+         iParent = i.parent
+         jParent = j.parent
+         while iParent != None and jParent != None and iParent.name == jParent.name:
+            iParent = iParent.parent
+            jParent = jParent.parent
+         if iParent == None and jParent == None:
+            return True
+      return False
+      
